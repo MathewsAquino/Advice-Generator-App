f0869cf19af483cf3defde9f0ff41f39e6b42168
Matheus Aquino <110079411+MathewsAquino@users.noreply.github.com>
2022-11-04 01:19:45 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const fetchAdvice = async () => {
-    const res = await fetch("https://api.adviceslip.com/advice")
-    const data = await res.json()
-  
-    document.querySelector(".title").innerHTML = `Advice #${data.slip.id}`
-    document.querySelector(".text").innerHTML = `"${data.slip.advice}"`
-  }
-  
-  fetchAdvice()
